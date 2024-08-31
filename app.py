61a1d5d205994acb55cbafc917cf142163992426
shaun craven <craven6622@googlemail.com>
2024-08-31 12:55:27 +0000

    images updated on index, mongodb set up

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import os
 import json
-from flask import Flask, render_template, request, flash
+from flask import (
+    Flask, render_template, redirect, 
+    request, session, flash, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
@@ -17,6 +28,13 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html", page_title="About Us")
+
+
+@app.route("/")
+@app.route("/get_categories")
+def get_categories():
+    categories = mongo.db.categories.find()
+    return render_template("categories.html", categories=categories)
 
 
 @app.route("/contact", methods=["GET", "POST"])
